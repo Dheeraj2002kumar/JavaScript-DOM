46f3202a4c03ab2c4b1f8c596ae29950d8e859c7
Dheeraj Kumar <dk8549644@gmail.com>
2025-03-31 09:06:37 +0530

    Add event listener to change paragraph text on button click

--- js-DOM/script.js
+++ js-DOM/script.js
@@ -1,4 +1,6 @@
 // Example 1
 
-let hold = document.getElementById('changeTextButton');
-console.log(hold);
+document.getElementById('changeTextButton').addEventListener("click", function(){
+    let paragraph = document.getElementById("myParagraph");
+    paragraph.textContent = "The paragraph changed.";
+})
