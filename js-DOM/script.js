bc7449874fc935a7d761240ab9b8ac9bd41ca1e1
Dheeraj Kumar <dk8549644@gmail.com>
2025-03-31 09:23:50 +0530

    Add event listener to highlight the first city in the list on button click

--- js-DOM/script.js
+++ js-DOM/script.js
@@ -4,3 +4,10 @@ document.getElementById('changeTextButton').addEventListener("click", function()
     let paragraph = document.getElementById("myParagraph");
     paragraph.textContent = "The paragraph changed.";
 })
+
+// Example 2
+
+document.getElementById("highlightFirstCity").addEventListener("click", function(){
+    let citiesList = document.getElementById("citiesList");
+    citiesList.firstElementChild.classList.add("first-list");
+})
